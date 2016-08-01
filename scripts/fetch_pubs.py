470a50a0b139ebbd9a24fdd0779f5b32ee4dc9c0
Dan Cook <danielecook@gmail.com>
2016-08-01 11:02:00 -0500

    Added Shannon's Paper

--- scripts/fetch_pubs.py
+++ scripts/fetch_pubs.py
@@ -4,37 +4,35 @@ import yaml
 
 # Add new PMIDS to publications.txt then run this script
 
-#  load current file
-with open('../_data/pubs_data.yaml', 'r') as q:
-	current_file = yaml.load(q)
-
 q = PubMedFetcher()
 
 # open the things to be updated from the publications.txt file
-pub_list = open("../pub/publications_list.txt", 'r').read().splitlines()[1:]
+pub_list = map(int, open("../publications/publications_list.txt", 'r').read().splitlines()[1:])
+
 
 def fetch_pub(pmid):
     pub = q.article_by_pmid(pmid)
     # only grab relevant information
     dpub = {'Title': pub.title, 'Authors': pub.authors, 'DOI': pub.doi, 'Date_Published': pub.year, 'Journal': pub.journal, 'PMC': pub.pmc, 'PMID': pub.pmid}
-
     return dpub
 
-# first entry isn't in correct format and doesnt have a PMID
-# del current_file[0]
-PMID_list = []
 doc_list = []
 
-# grab all PMIDs
-for i, entry in enumerate(current_file):
-	if 'PMID' not in entry:
-		doc_list.append(entry)
-	if 'PMID' in entry:
-		if 'DOI' in entry:
-			doc_list.append(entry)
-		else:
-			doc_list.append(fetch_pub(str(entry['PMID']))) 
+#  load current file
+with open('../_data/pubs_data.yaml', 'r') as f:
+    yaml_db = yaml.load(f)
 
+existing_pmids = [int(x["PMID"]) for x in yaml_db if 'PMID' in x]
+
+for i in pub_list:
+    print(i)
+    if i not in existing_pmids:
+        doc_list.append(fetch_pub(i))
+
+for entry in yaml_db:
+    doc_list.append(entry)
+    
+print(doc_list)
 
 with open("../_data/pubs_data2.yaml", 'w') as f:
-		f.write(yaml.safe_dump(doc_list))
+        f.write(yaml.safe_dump(doc_list))
